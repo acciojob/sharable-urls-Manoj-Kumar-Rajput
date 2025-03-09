4bed5f4491ff6263797f6206ea767a6af512de74
Manoj-Kumar_Rajput <93696591+Manoj-Kumar-Rajput@users.noreply.github.com>
2025-03-10 01:52:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+function submit()
+{
+	//doument.innerHTML="";
+	let name=document.getElementById("name").value;
+	let year=document.getElementById("year").value;
+	let h3=document.getElementById("h3");
+	url="https://localhost:8080";
+	if(name!="" && year!="")
+	{
+		h3.innerText=url+"?name="+name+"&year="+year;
+	}
+	else if(name!="" && year=="")
+	{
+		h3.innerText=url+"?name="+name;
+}
+	else if(name=="" && year!="")
+	{
+h3.innerText=url+"?year="+year;
+}
+else
+{
+	h3.innerText=url+"/";
+
+}
+
+}
