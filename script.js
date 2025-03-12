b9cadbd107f8ff68fe5903d9db16482df8553c68
Manoj-Kumar_Rajput <93696591+Manoj-Kumar-Rajput@users.noreply.github.com>
2025-03-12 22:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,21 +6,18 @@ button.addEventListener("click",()=>
 	let name=document.getElementById("name").value;
 	let year=document.getElementById("year").value;
 	let h3=document.getElementById("url");
-	let url="https://localhost:8080/";
+	
 	if(name!="" && year!="")
 	{
-		h3.innerText=url+"?name="+name+"&year="+year;
+		h3.innerText+="?name="+name+"&year="+year;
 	}
 	else if(name!="" && year=="")
 	{
-		h3.innerText=url+"?name="+name;
+		h3.innerText+="?name="+name;
 }
 	else if(name=="" && year!="")
 	{
-h3.innerText=url+"?year="+year;
+h3.innerText+="?year="+year;
 }
-    else
-	{
-		h3.innerText=url;
-	}
+    h3.style.display="block";
 });
